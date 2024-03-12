431e6c990414b79c4ab62cf51de69ab4c4249cd3
Kamila5oft <kgrana.paellasoft@gmail.com>
2024-03-12 16:09:23 +0100

    search-bar configuration

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,62 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
   searchTerm: string = '';
+  allPokemons: any[] = [];
+  filteredPokemons: any[] = [];
+
+  isSearching = false;
+  isLoading = false;
+
+  constructor(private snackBar: MatSnackBar, private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.loadPokemons();
+  }
+
+  onSearch(): void {
+    const value = this.searchTerm.toLowerCase();
+    if(value === '') {
+      this.filteredPokemons	= this.allPokemons;
+      this.isSearching = false;
+      this.isLoading = false;
+    } else {
+      this.isSearching = true;
+      this.isLoading = true;
+
+      this.filteredPokemons = this.allPokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(value)
+        );
+
+        this.isLoading = false;
+
+        if(this.filteredPokemons.length === 0) {
+          this.snackBar.open('Sorry, Pokemon not found', 'Ok', {
+            duration: 5000,
+          });
+        }
+    }
+  }
+
+
+  loadPokemons(): void {
+    this.pokemonService.getAllPokemon().subscribe(data => {
+      this.allPokemons = data;
+      this.filteredPokemons = [...this.allPokemons];
+      this.isLoading = false;
+    }, error => {
+      this.isLoading = false;
+      console.error('Error loading Pokemons:', error)
+    });
+  }
+
 
 }
